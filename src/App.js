c416afdf13cb7d5303a7b93838f170efbfb7ac5e
Leo Omaña <leotangram@gmail.com>
2022-01-15 15:45:01 -0500

    Generic integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import MarketingApp from './components/MarketingApp'
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <hr />
+      <MarketingApp />
     </div>
   )
 }
